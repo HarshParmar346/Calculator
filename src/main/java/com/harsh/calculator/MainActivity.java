a79c71f2d1a9a361abbf4189315a35f026e98f4c
Harsh Parmar <89838217+HarshParmar346@users.noreply.github.com>
2021-08-31 20:56:38 +0530

    Add files via upload

---
+++ src/main/java/com/harsh/calculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.harsh.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+}
